fdd7d702289f6ccef47c58a95f2486728be87614
rksathsara-netizen <rksathsara@gmail.com>
2026-08-06 15:23:00 +0530

    Piliyandala

--- src/data/site.ts
+++ src/data/site.ts
@@ -40,7 +40,7 @@ export const business = {
   phone: '+94 75 678 9046',
   phoneRaw: '94756789046',
   email: 'rksolution.lk@gmail.com',
-  address: 'No. 45, Colombo Road, Piliyandala 10300, Sri Lanka',
+  address: 'Piliyandala, Sri Lanka',
   addressShort: 'Piliyandala, Sri Lanka',
   hours: 'Mon–Sat: 9AM – 8PM | Sun: 10AM – 5PM',
   mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31686.5!2d79.92!3d6.80!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNDgnMDYuNCJOIDc5wrA1NSc0MS43IkU!5e0!3m2!1sen!2slk!4v1700000000000',
