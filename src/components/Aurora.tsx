1781bfda3fcd9616f869161513bb65a241ab67a1
rksathsara-netizen <rksathsara@gmail.com>
2026-08-10 13:03:49 +0530

    qwq

--- src/components/Aurora.tsx
+++ src/components/Aurora.tsx
@@ -1,6 +1,6 @@
 export function Aurora() {
   return (
-    <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none hidden md:block">
+    <div className="hidden md:block fixed inset-0 -z-10 overflow-hidden pointer-events-none">
       <div className="absolute -top-1/4 -left-1/4 w-[60vw] h-[60vw] rounded-full bg-primary-600/20 blur-[120px] animate-aurora" />
 
       <div className="absolute top-1/3 -right-1/4 w-[50vw] h-[50vw] rounded-full bg-accent-500/15 blur-[100px] animate-aurora-2" />
